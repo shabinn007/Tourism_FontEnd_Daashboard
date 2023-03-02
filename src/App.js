138e8b2824d2bc15b45f814b51761d267bba85c7
shabin007 <shabinshaw@gmail.com>
2023-03-02 11:23:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+/** @format */
 
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
-import SignUp from './components/Authenticattion/SignUp'
-import Login from './components/Authenticattion/Login'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./components/Authenticattion/SignUp";
+import Login from "./components/Authenticattion/Login";
 
-import './App.css'
+import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Places from './components/places'
-import Tickets from './components/tickets'
-import Customers from './components/customers';
+import Places from "./components/places";
+import Tickets from "./components/tickets";
+import Customers from "./components/customers";
 function App() {
   return (
     <div>
@@ -15,22 +16,19 @@ function App() {
       <div className="container">
         <SideBar />
         <Home/> */}
-         <BrowserRouter>
-    <Routes>
-      {/* <Route path='/' element={<LandingPage/>}/>      ..........Build this .......... */}
-      <Route path='/home' element={<Dashboard/>}/> 
-      <Route path='/places' element={<Places/>}/> 
-      <Route path='/tickets' element={<Tickets/>}/> 
-      <Route path='/customers' element={<Customers/>}/> 
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/' element={<LandingPage/>}/>      ..........Build this .......... */}
+          <Route path="/home" element={<Dashboard />} />
+          <Route path="/places" element={<Places />} />
+          <Route path="/tickets" element={<Customers />} />
+          <Route path="/customers" element={<Tickets />} />
 
-
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
-    </BrowserRouter> 
-
-      </div>
-    
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
